f25f18049125d4de86607fc326b2463ccffadfe2
Nick <idainswan@gmail.com>
2025-10-20 13:31:50 +0300

    добавил поле с изображением в товаре / метод отображения мини-изображения прямо в админ панеле для удобства / подключил media в urls для возможности добавления админом изображений через панель

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -27,6 +27,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
     available = models.BooleanField(default=True)
+    image = models.ImageField(
+        upload_to='products/%Y/%m/%d/',
+        blank=True,
+        null=True,
+        verbose_name='Изображение'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
