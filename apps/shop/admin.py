a8bceb981304a0abc57ca1211b55c69f3f03ebdc
Nick <idainswan@gmail.com>
2025-10-20 15:44:55 +0300

    изменил модели, добавил поле с функционалом добавления нескольких изображений к одному товару, убрал модели корзин / выполнил миграции заново, зарегистрировал модели в admin

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import Category, Product, Cart, CartItem
+from .models import Category, Product, ProductImage
 
 admin.site.site_header = "Панель управления магазином"
 admin.site.site_title = "Магазин"
@@ -9,42 +9,34 @@ admin.site.index_title = "Администрирование магазина"
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
-    search_fields = ['name']
-    ordering = ['name']
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ["name"]
+    ordering = ["name"]
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['image_tag', 'name', 'price', 'stock', 'available', 'category']
-    list_filter = ['available', 'category']
-    prepopulated_fields = {'slug': ('name',)}
-    search_fields = ['name', 'description']
-    list_editable = ['price', 'stock', 'available']
-    ordering = ['name']
+    list_display = ["image_tag", "name", "price", "available", "category"]
+    list_filter = ["available", "category", "brand", "color", "size", "age_group"]
+    search_fields = ["name", "description", "brand", "color", "size"]
+    list_editable = ["price", "available"]
+    ordering = ["name"]
 
-    readonly_fields = ['image_tag']
+    readonly_fields = ["image_tag"]
+    inlines = [ProductImageInline]
 
     def image_tag(self, obj):
         if obj.image:
-            return format_html('<img src="{}" width="50" height="50" style="object-fit: cover;"/>', obj.image.url)
+            return format_html(
+                '<img src="{}" width="50" height="50" style="object-fit: cover; border-radius: 6px;"/>',
+                obj.image.url,
+            )
         return "-"
 
-    image_tag.short_description = 'Изображение'
-
-
-class CartItemInline(admin.TabularInline):
-    model = CartItem
-    extra = 0
-
-
-@admin.register(Cart)
-class CartAdmin(admin.ModelAdmin):
-    list_display = ['user', 'created_at']
-    inlines = [CartItemInline]
-
-
-@admin.register(CartItem)
-class CartItemAdmin(admin.ModelAdmin):
-    list_display = ['cart', 'product', 'quantity']
+    image_tag.short_description = "Изображение"
