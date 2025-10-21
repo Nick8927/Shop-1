932b4d7ac475b97b38d23c90f453ef478b43749c
Nick <idainswan@gmail.com>
2025-10-21 16:37:05 +0300

    промежуточные правки фронта

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import CategoryListAPIView, ProductListAPIView, ProductDetailAPIView
+from .views import (
+    CategoryListAPIView, ProductListAPIView, ProductDetailAPIView,
+    IndexView, ProductDetailView
+)
 
 urlpatterns = [
-    path('categories/', CategoryListAPIView.as_view(), name='api-categories'),
-    path('products/', ProductListAPIView.as_view(), name='api-products'),
-    path('products/<int:pk>/', ProductDetailAPIView.as_view(), name='api-product-detail'),
+    path('', IndexView.as_view(), name='index'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+
+
+    path('api/categories/', CategoryListAPIView.as_view(), name='api-categories'),
+    path('api/products/', ProductListAPIView.as_view(), name='api-products'),
+    path('api/products/<int:pk>/', ProductDetailAPIView.as_view(), name='api-product-detail'),
 ]
